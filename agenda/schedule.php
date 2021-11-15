e6e553a00a35120c5a534bf0b1fcb3d7c9717f70
pau13-loop <pllinas@cifpfbmoll.eu>
2021-11-15 18:57:14 +0100

    feat: :sparkles: Design the html schema for the agenda and paint the data saved inside the schedule array

--- agenda/schedule.php
+++ agenda/schedule.php
@@ -8,64 +8,46 @@
 
 <body>
     <?php
-    //! Alert messages
-    $error_new_name_field = false;
+    $_schdeule = [
+        "Pau" => "661610908",
+        "Papi" => "677394680"
+    ];
 
-    if (isset($_GET['schedule'])) {
-        // Con isset() comprobamos que la variable no sea null y contenga algún valor
-        $schedule = $_GET['schedule'];
-    } else {
-        $schedule = array();
-        // En el caso de ser null, creamos un array vacia y se la asignamos a agenda
-    }
-
-    // onSubmit
-    if (isset($_GET['submit'])) {
-        $new_name = filter_input(INPUT_GET, 'name');
-        $new_telephone = filter_input(INPUT_GET, 'telephone');
-
-        if (empty($new_name)) {
-            $error_new_name_field = true;
-        } elseif (empty($new_telephone)) {
-            unset($agenda[$nuevo_nombre]);
-        } else {
-            $schedule[$new_name] = $new_telephone;
-        }
-    }
     ?>
-    <div style="display: table-caption; margin-left: 15px">
-        <h3>New Schedule Contact</h3>
-        <form>
-            <?php
-            foreach ($schedule as $name => $telephone) {
-                echo '<input type="hidden" name="schedule[' . $name . ']"';
-                echo 'value="' . $telephone . '"/>';
-            }
-            ?>
-            <label>Name: <input type="text" name="name" style="margin-bottom: 10px" /></label>
-            <?php
-            echo $error_new_name_field ? "<p style='color:red; margin: 0px 0px 10px 0px; font-size: 14px '>Yous must fill the name field !</p>" : null;
-            ?>
-            <label>Telephone: <input type="number" name="telephone" /></label>
-            <input style="margin-top: 20px" type="submit" name="submit" value="execute" />
-        </form>
-    </div>
+    <div style="text-align:center;">
+        <div>
+            <h2>
+                Form Schedule
+            </h2>
+            <form>
+                <div style="margin-bottom:15px;">
+                    <label for="name">First name:</label><br>
+                    <input type="text" id="name" name="name" style="border-radius:5px;height: 20px;background-color:aquamarine;"><br>
+                </div>
+                <div style="margin-bottom:15px;">
+                    <label for="phone">Phone number:</label><br>
+                    <input type="text" id="phone" name="phone" style="border-radius:5px;height:20px;background-color:bisque;"><br>
+                </div>
+                <input type="submit" value="Save contact" style="border-radius:5px;height:30px;width:10%;">
+            </form>
+        </div>
+        <div style=margin-top:30px;>
+            <h3>Saved Contacts</h3>
 
-    <div style="margin-left: 15px">
-        <h3>Schedule</h3>
-        <?php
-        if (empty($schedule)) {
-            echo "<p>The schedule has got no contacts :(</p>";
-        } else {
-            echo "<ul>";
-            foreach ($schedule as $name => $tel) {
-                echo "<li>" . $name . " : " . $tel . "</li>";
-            }
-            echo "</ul>";
-        }
-        ?>
-    </div>
+            <ul style="margin-right:45px;list-style:inside;">
+                <?php
+                if (count($_schdeule) > 0) {
+                    foreach ($_schdeule as $key => $value) {
+                        echo "<li>{$key} -- {$value}</li>";
+                    }
+                } else {
+                    echo "<p>The schedule doesn't have contacts saved !</p>";
+                }
+                ?>
+            </ul>
 
+        </div>
+    </div>
 </body>
 
 </html>
