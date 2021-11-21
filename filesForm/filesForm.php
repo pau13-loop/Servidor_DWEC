40e2532f25315364384a69640098a3f09e9f77c8
pau13-loop <pllinas@cifpfbmoll.eu>
2021-11-21 18:21:18 +0100

    feat: :sparkles: Create simple form with PHP to upload files

---
+++ filesForm/filesForm.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form method="post" enctype="multipart/form-data">
+            <input type="file" name="fileToUpload1" id="fileToUpload1">
+            <input type="file" name="fileToUpload2" id="fileToUpload2">
+            
+            <input type="submit" value="Upload Files" name="submit">
+        </form>
+    </body>
+</html>
